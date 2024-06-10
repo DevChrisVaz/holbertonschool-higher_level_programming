0cebedb84826fe38ea9ec87f3e185b4642d4fec7
Christian Alexander Vazquez Gonzalez <dev.chrisvaz@gmail.com>
2024-06-10 12:59:28 -0600

    python-more_data_structures

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# Christian Alexander Vazquez Gonzalez <dev.chrisvaz@gmail.com>
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
