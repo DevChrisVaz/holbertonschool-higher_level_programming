81093e503d7cf0ae1f52fc941620198312ef25bb
Christian-Macropay <christian.vazquez@macropay.mx>
2024-06-26 18:28:42 -0600

    more classes

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Christian Alexander Vazquez Gonzalez <dev.chrisvaz@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+iudsbfkjsbdfijsdbf
