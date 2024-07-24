01c1cf95c75b7747d1b42cbe36f3091475ab674a
Christian-Macropay <christian.vazquez@macropay.mx>
2024-07-23 18:56:07 -0600

    ready to test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     rows = cur.fetchall()
 
     for row in rows:
-        print(row)
+        print(f"({row.id}, '{row.name}')")
         
     # Close the cursor and the connection
     cur.close()
